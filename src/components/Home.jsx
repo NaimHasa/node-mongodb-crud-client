94640018425705c3cb85357bee067c92b0425ae0
Naim Hasan <naimarafat7@gmail.com>
2023-07-18 12:16:52 -0700

    create read and delete

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users);
     const handleDeleted = user => {
         // console.log('deleting user', user);
         const agree = window.confirm(`Are U Sure You will be Deleted user: ${user.name}`);
@@ -12,7 +13,16 @@ const Home = () => {
                 method: 'DELETE'
             })
                 .then(res => res.json())
-                .then(data => console.log(data))
+                .then(data => {
+                    console.log(data)
+                    if (data.deletedCount > 0) {
+                        alert('User Deleted successfully');
+
+                        const remainingUsers = displayUsers.filter(usr => usr._id !== user._id);
+                        setDisplayUsers(remainingUsers);
+                    }
+
+                })
 
         }
 
@@ -22,10 +32,10 @@ const Home = () => {
 
     return (
         <div>
-            <h1>Total Users: {users.length}</h1>
+            <h1>Total Users: {displayUsers.length}</h1>
 
             {
-                users.map(user =>
+                displayUsers.map(user =>
                     <p key={user._id}>
                         {user.name}  {user.email}
 
